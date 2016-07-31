8405db595a57d7933b919aa28a7d976b113ce115
SBeator <star_yes@qq.com>
2016-07-31 18:37:26 +0800

    Simple use

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+// Gruntfile
+module.exports = function (grunt) {
+  grunt.initConfig({
+    webfont: {
+      icons: {
+        src: 'icons/*.svg',
+        dest: 'build/fonts',
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-webfont');
+
+  grunt.registerTask('default', ['webfont']);
+};
